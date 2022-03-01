514287712fba1cdc0916095a5f3aad92d23404dc
Rounak Saraf <46487512+Rounak183@users.noreply.github.com>
2022-03-02 05:13:46 +0530

    Add files via upload

---
+++ src/MyComponents/About.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export const About = () => {
+    return (
+        <div>
+            This is an about component 
+            <p>This is just to show that my page does not reload on switching to different tabs.</p>
+        </div>
+    )
+}
